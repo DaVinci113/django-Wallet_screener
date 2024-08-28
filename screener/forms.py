22c0aab9af3660c7176b6f4d5226ec312fab3bcd
DaVinci <maxkgd@mail.ru>
2024-08-28 16:09:41 +0200

    перенос try в clean addressform

--- screener/forms.py
+++ screener/forms.py
@@ -16,20 +16,19 @@ class AdressForm(forms.ModelForm):
     def clean_address(self):
         cleaned_data = super().clean()
         input_address = cleaned_data.get('address')
-        
-        if input_address[:-39] not in list_of_support_chain:
-            raise ValidationError('Unsupported chain or incorrect address!')
-        
         try:
+            if input_address[:-39] not in list_of_support_chain:
+                raise ValidationError('Unsupported chain or incorrect address!')
+
             address = Address.objects.get(address=input_address)
             wallet_id = address.wallet_id
             wallet = Wallet.objects.get(pk=wallet_id)
             addresses = wallet.address_set.all()
             if any(addr for addr in addresses if str(addr) == input_address):
                 raise ValidationError('Address is duplicate in this wallet!')
-            
+
         except Address.DoesNotExist:
-            return cleaned_data.get('address')
+            return cleaned_data.get('address'), cleaned_data.get('wallet_id')
     
     class Meta:
         model = Address
